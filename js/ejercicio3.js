fa1bdac9104c09211df892e72abb0db97479756e
claudiapalazongomez <98claudia@gmail.com>
2023-11-26 08:59:22 +0100

    add exercise 3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,15 +1,17 @@
+/*JQUERY*/
+// Esperamos a que el documento se cargue antes de ejecutar el código JQuery
 $(document).ready(function () {
   $("#agregarLinea").click(function (event) {
-    event.preventDefault(); // Evita el comportamiento por defecto del enlace
+    event.preventDefault(); // Evitamos el comportamiento por defecto del enlace
 
     // Obtener los valores de los campos de entrada
-    const producto = $("#producto").val();
-    const cantidad = $("#cantidad").val();
-    const precioUnitario = $("#precio-unitario").val();
-    const descuento = $("#descuento").val();
+    let producto = $("#producto").val();
+    let cantidad = $("#cantidad").val();
+    let precioUnitario = $("#precio-unitario").val();
+    let descuento = $("#descuento").val();
 
-    // Crear una nueva fila con los valores obtenidos y el botón
-    const nuevaFila = `<tr>
+    // Crear una nueva fila con los valores obtenidos y un botón que eliminará esa fila al clicar en él
+    let nuevaFila = `<tr>
                             <td>${producto}</td>
                             <td>${cantidad}</td>
                             <td>${precioUnitario}</td>
@@ -20,16 +22,16 @@ $(document).ready(function () {
     // Agregar la nueva fila al final de la tabla
     $("#cuerpo-tabla").append(nuevaFila);
 
-    // Limpiar los campos después de agregar la fila
+    // Limpiar los campos de los inputs después de agregar la fila
     $("#producto").val("");
     $("#cantidad").val("");
     $("#precio-unitario").val("");
     $("#descuento").val("");
   });
 
-  // Función para eliminar la fila al hacer clic en el botón "Eliminar"
+  // Función para eliminar la fila al hacer clic en el botón Eliminar
   $("#cuerpo-tabla").on("click", ".eliminarLinea", function (event) {
-    event.preventDefault();
+    event.preventDefault(); // Evitamos el comportamiento por defecto del enlace de nuevo
     $(this).closest("tr").remove();
   });
 });
